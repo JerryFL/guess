bbba56da622c4c2d61fd2c4e2dcb0ebf20471bfb
JerryFL <3573764836@qq.com>
2020-01-15 15:49:31 +0800

    guess

--- caishuzi.py
+++ caishuzi.py
@@ -1,9 +1,14 @@
 import random
-
-r = random.randint(1, 100)
+start = input('请决定随机数字的范围的开始值')
+end = input('请决定随机数字范围的结束值')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
+count = 0
 while True:
-    num = input('请猜数字： ')
-    num = int(num)
+	count = count + 1
+    num=input('请输入随机数字：')
+    num=int(num)
     if num == r:
         print('你猜中了')
         break
@@ -11,4 +16,5 @@ while True:
         print('比答案大')
     elif num < r:
         print('比答案小')
+    print('这是你猜的第', count,'次')
 
